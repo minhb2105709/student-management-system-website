df81c73d1c3ec45720d1bbc53933aa05b5a55709
kbminh <minhb2105709@student.ctu.edu.vn>
2024-10-08 23:32:51 +0700

    create database

--- backend/src/database/student_management.sql
+++ backend/src/database/student_management.sql
@@ -1,3 +1,18 @@
 create database studentManagement default charset utf8mb4;
 use studentManagement;
 
+create table roles (
+	role_ID int primary key,
+    role_name varchar(255)
+);
+
+insert into roles values (0, 'admin'), (2, 'lecturer'), (1, 'student');
+
+-- Tài Khoản
+create table accounts (
+	acc_ID int primary key auto_increment,
+    acc_username varchar(255) unique not null,
+    acc_password varchar(255) not null,
+    role_ID int default 1,
+    foreign key (role_ID) references roles (role_ID)
+);
